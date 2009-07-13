84c79b2958bc3957cdc3f37d06892d2b367e585f
yoneda <yoneda@debian.localnet.mda.or.jp>
2009-07-13 23:46:20 +0900

    support mediatomb db update (testing)

--- recomplete.php
+++ recomplete.php
@@ -11,6 +11,20 @@ try{
 	if( file_exists( INSTALL_PATH . SPOOL . "/". $rrec->path ) ) {
 		// 予約完了
 		$rrec->complete = '1';
+		if( MEDIATOMB_UPDATE) {
+			$dbh = mysql_connect( DB_HOST, DB_USER, DB_PASS );
+			if( $dbh !== false ) {
+				$sqlstr = "use ".DB_NAME;
+				mysql_query( $sqlstr );
+				// 別にやらなくてもいいが
+				$sqlstr = "set NAME utf8";
+				mysql_query( $sqlstr );
+				$sqlstr = "update mt_cds_object set metadata='dc:description=".$rrec->description."' where dc_title='".$rrec->path."'";
+				mysql_query( $sqlstr );
+				$sqlstr = "update mt_cds_object set dc_title='".$rrec->title."' where dc_title='".$rrec->path."'";
+				mysql_query( $sqlstr );
+			}
+		}	
 	}
 	else {
 		// 予約失敗
