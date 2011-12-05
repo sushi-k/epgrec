f93bfd5a39fd8ac0baa1ebeceb96e9a62eca05a2
ha1t <halt.feits@gmail.com>
2011-12-05 12:13:08 +0900

    録画済み一覧を最低限動くように

--- recordedTable.php
+++ recordedTable.php
@@ -6,23 +6,22 @@ include_once( INSTALL_PATH . '/Settings.class.php' );
 
 $settings = Settings::factory();
 
-
 $order = "";
 $search = "";
 $category_id = 0;
 $station = 0;
 
-// mysql_real_escape_stringより先に接続しておく必要がある
-$dbh = @mysql_connect( $settings->db_host, $settings->db_user, $settings->db_pass );
-
 // $options = "WHERE complete='1'";
 $options = "WHERE starttime < '". date("Y-m-d H:i:s")."'";	// ながら再生は無理っぽい？
 
-if(isset( $_GET['key']) ) {
-	$options .= " AND autorec ='".mysql_real_escape_string(trim($_GET['key']))."'";
+$row = array();
+$where = '';
+if (isset($_GET['key'])) {
+    $where = 'autorec = ?';
+    $row['autorec'] = $_GET['key'];
 }
 
-if(isset( $_POST['do_search'] )) {
+if (isset($_POST['do_search'])) {
 	if( isset($_POST['search'])){
 		if( $_POST['search'] != "" ) {
 			$search = $_POST['search'];
@@ -43,70 +42,31 @@ if(isset( $_POST['do_search'] )) {
 	}
 }
 
-
-$options .= " ORDER BY starttime DESC";
-
+$db = DB::conn();
 try{
-	$rvs = DBRecord::createRecords(RESERVE_TBL, $options );
-	$records = array();
-	foreach( $rvs as $r ) {
-		$cat = new DBRecord(CATEGORY_TBL, "id", $r->category_id );
-		$ch  = new DBRecord(CHANNEL_TBL,  "id", $r->channel_id );
-		$arr = array();
-		$arr['id'] = $r->id;
-		$arr['station_name'] = $ch->name;
-		$arr['starttime'] = $r->starttime;
-		$arr['endtime'] = $r->endtime;
-		$arr['asf'] = "".$settings->install_url."/viewer.php?reserve_id=".$r->id;
-		$arr['title'] = htmlspecialchars($r->title,ENT_QUOTES);
-		$arr['description'] = htmlspecialchars($r->description,ENT_QUOTES);
-		$arr['thumb'] = "<img src=\"".$settings->install_url.$settings->thumbs."/".$r->path.".jpg\" />";
-		$arr['cat'] = $cat->name_en;
-		$arr['mode'] = $RECORD_MODE[$r->mode]['name'];
-		
-		array_push( $records, $arr );
-	}
-	
-	$crecs = DBRecord::createRecords(CATEGORY_TBL );
-	$cats = array();
-	$cats[0]['id'] = 0;
-	$cats[0]['name'] = "すべて";
-	$cats[0]['selected'] = $category_id == 0 ? "selected" : "";
-	foreach( $crecs as $c ) {
-		$arr = array();
-		$arr['id'] = $c->id;
-		$arr['name'] = $c->name_jp;
-		$arr['selected'] = $c->id == $category_id ? "selected" : "";
-		array_push( $cats, $arr );
-	}
-	
-	$crecs = DBRecord::createRecords(CHANNEL_TBL );
-	$stations = array();
-	$stations[0]['id'] = 0;
-	$stations[0]['name'] = "すべて";
-	$stations[0]['selected'] = (! $station) ? "selected" : "";
-	foreach( $crecs as $c ) {
-		$arr = array();
-		$arr['id'] = $c->id;
-		$arr['name'] = $c->name;
-		$arr['selected'] = $station == $c->id ? "selected" : "";
-		array_push( $stations, $arr );
-	}
-	
-	
-	$smarty = new Smarty();
-	$smarty->assign("sitetitle","録画済一覧");
-	$smarty->assign( "records", $records );
-	$smarty->assign( "search", $search );
-	$smarty->assign( "stations", $stations );
-	$smarty->assign( "cats", $cats );
-	$smarty->assign( "use_thumbs", $settings->use_thumbs );
-	
-	$smarty->display("recordedTable.html");
-	
-	
+    $rows = $db->rows('SELECT * FROM Recorder_reserveTbl LEFT JOIN Recorder_programTbl ON Recorder_reserveTbl.program_disc = Recorder_programTbl.program_disc ORDER BY starttime DESC');
+    //$rvs = DBRecord::createRecords(RESERVE_TBL, $options );
+    $records = array();
+    foreach ($rows as $key => $r) {
+        $cat = new DBRecord(CATEGORY_TBL, "category_disc", $r['category_disc']);
+        $ch  = new DBRecord(CHANNEL_TBL,  "channel_disc", $r['channel_disc']);
+        //$rows[$key]['asf'] = "".$settings->install_url."/viewer.php?reserve_id=".$r->id;
+        //$rows[$key]['thumb'] = "<img src=\"".$settings->install_url.$settings->thumbs."/".$r->path.".jpg\" />";
+        //$rows[$key]['cat'] = $cat->name_en;
+    }
+
+    $categories = $db->rows('SELECT * FROM Recorder_categoryTbl');
+    $channels = $db->rows('SELECT * FROM Recorder_channelTbl');
+
+    $smarty = new Smarty();
+    $smarty->assign("sitetitle","録画済一覧");
+    $smarty->assign("records", $rows);
+    $smarty->assign("search", $search);
+    $smarty->assign("channels", $channels);
+    $smarty->assign("categories", $categories);
+    $smarty->assign("use_thumbs", $settings->use_thumbs );
+
+    $smarty->display("recordedTable.html");
+} catch (exception $e) {
+    throw $e;
 }
-catch( exception $e ) {
-	exit( $e->getMessage() );
-}
-?>
