7ab55b8c74414ba7a86575210b379d327384948b
ha1t <halt.feits@gmail.com>
2011-12-05 15:34:10 +0900

    不要部分の削除

--- deleteKeyword.php
+++ deleteKeyword.php
@@ -1,18 +1,14 @@
 <?php
-include_once('config.php');
-include_once( INSTALL_PATH . '/DBRecord.class.php' );
-include_once( INSTALL_PATH . '/Reservation.class.php' );
-include_once( INSTALL_PATH . '/reclib.php' );
-include_once( INSTALL_PATH . '/Keyword.class.php' );
+require_once 'config.php';
+require_once INSTALL_PATH . '/Keyword.class.php';
 
-if( isset($_GET['keyword_id'])) {
-	try {
-		$rec = new Keyword( "id", $_GET['keyword_id'] );
-		$rec->delete();
-	}
-	catch( Exception $e ) {
-		exit( "Error:" . $e->getMessage() );
-	}
+if (!isset($_GET['keyword_id'])) {
+    exit("Error:キーワードIDが指定されていません");
 }
-else exit( "Error:キーワードIDが指定されていません" );
-?>
+
+try {
+    $rec = new Keyword( "id", $_GET['keyword_id']);
+    $rec->delete();
+} catch(Exception $e ) {
+    exit("Error:" . $e->getMessage());
+}
