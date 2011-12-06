7da424e6f16c4274aa1f14ee68eca0d924f24e7b
ha1t <halt.feits@gmail.com>
2011-12-06 15:37:45 +0900

    channelInfoをapi.phpに移動

--- api.php
+++ api.php
@@ -31,11 +31,45 @@ class API_Controller
         $smarty->assign('url', 'index.php');
         $smarty->display("dialog.html");
     }
+
+    public function channelInfo()
+    {
+        require_once( INSTALL_PATH . "/DBRecord.class.php" );
+        if( isset($_GET['channel_disc']) ) {
+
+            try {
+                $crec = new DBRecord( CHANNEL_TBL, "channel_disc", $_GET['channel_disc'] );
+
+                echo '<div class="prg_title">';
+                echo $crec->name . "</div>";
+
+                // 種別
+                echo '<div class="prg_channel"><span class="labelLeft">種別：</span><span class="bold">';
+                echo $crec->type;
+                echo '</span></div>';
+
+                // チャンネル
+                echo '<div class="prg_channel"><span class="labelLeft">物理チャンネル：</span><span class="bold">';
+                echo $crec->channel;
+                echo '</span></div>';
+
+                // フォーム
+                echo '<form method="post" action="channelSetSID.php">';
+                echo '<div class="prg_channel"><span class="labelLeft">サービスID：</span>';
+                echo '<span><input type="text" name="n_sid" size="20" id="id_sid" value="'. $crec->sid .'" /></span>';
+                echo '<input type="hidden" name="n_channel_disc" id="id_disc" value="'. $crec->channel_disc .'" />';
+                echo '</div>';
+                echo '</form>';
+            } catch( Exception $e ) {
+                echo "error:チャンネル情報の取得に失敗";
+            }
+        }
+    }
 }
 
 // dispatch
 $controller = new API_Controller();
-if (in_array($_REQUEST['method'], array('simpleReservation', 'saveSettings'))) {
+if (in_array($_REQUEST['method'], array('simpleReservation', 'saveSettings', 'channelInfo'))) {
     $controller->$_REQUEST['method']();
 }
 
