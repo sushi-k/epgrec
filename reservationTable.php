d6f9383e21f7b406fea11c037dc83e2a33db80b5
ha1t <halt.feits@gmail.com>
2011-12-11 10:47:23 +0900

    retab

--- reservationTable.php
+++ reservationTable.php
@@ -1,35 +1,33 @@
 <?php
-include_once('config.php');
-include_once( INSTALL_PATH . '/DBRecord.class.php' );
-include_once( INSTALL_PATH . '/Smarty/Smarty.class.php' );
+require_once 'config.php';
+require_once INSTALL_PATH . '/DBRecord.class.php';
+require_once INSTALL_PATH . '/Smarty/Smarty.class.php';
 
 try{
-	$rvs = DBRecord::createRecords(RESERVE_TBL, "WHERE complete='0' ORDER BY starttime ASC" );
-	
-	$reservations = array();
-	foreach( $rvs as $r ) {
-		$cat = new DBRecord(CATEGORY_TBL, "id", $r->category_id );
-		$arr = array();
-		$arr['id'] = $r->id;
-		$arr['type'] = $r->type;
-		$arr['channel'] = $r->channel;
-		$arr['starttime'] = $r->starttime;
-		$arr['endtime'] = $r->endtime;
-		$arr['mode'] = $RECORD_MODE[$r->mode]['name'];
-		$arr['title'] = $r->title;
-		$arr['description'] = $r->description;
-		$arr['cat'] = $cat->name_en;
-		$arr['autorec'] = $r->autorec;
-		
-		array_push( $reservations, $arr );
-	}
-	
-	$smarty = new Smarty();
-	$smarty->assign("sitetitle","録画予約一覧");
-	$smarty->assign( "reservations", $reservations );
-	$smarty->display("reservationTable.html");
+    $rvs = DBRecord::createRecords(RESERVE_TBL, "WHERE complete='0' ORDER BY starttime ASC" );
+
+    $reservations = array();
+    foreach( $rvs as $r ) {
+        $cat = new DBRecord(CATEGORY_TBL, "id", $r->category_id );
+        $arr = array();
+        $arr['id'] = $r->id;
+        $arr['type'] = $r->type;
+        $arr['channel'] = $r->channel;
+        $arr['starttime'] = $r->starttime;
+        $arr['endtime'] = $r->endtime;
+        $arr['mode'] = $RECORD_MODE[$r->mode]['name'];
+        $arr['title'] = $r->title;
+        $arr['description'] = $r->description;
+        $arr['cat'] = $cat->name_en;
+        $arr['autorec'] = $r->autorec;
+
+        array_push( $reservations, $arr );
+    }
+
+    $smarty = new Smarty();
+    $smarty->assign("sitetitle","録画予約一覧");
+    $smarty->assign( "reservations", $reservations );
+    $smarty->display("reservationTable.html");
+} catch (Exception $e) {
+    exit($e->getMessage());
 }
-catch( exception $e ) {
-	exit( $e->getMessage() );
-}
-?>
