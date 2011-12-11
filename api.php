5e0e6c2b7f7b66565365a355fafe4d73da047773
ha1t <halt.feits@gmail.com>
2011-12-11 11:33:16 +0900

    reservationformをapi.phpに

--- api.php
+++ api.php
@@ -81,6 +81,57 @@ class API_Controller
             exit("Error:" . $e->getMessage());
         }
     }
+
+    public function reservationForm()
+    {
+        require_once INSTALL_PATH . '/DBRecord.class.php';
+        require_once INSTALL_PATH . '/Smarty/Smarty.class.php';
+
+        if (!isset($_GET['program_id'])) {
+           exit('Error: 番組IDが指定されていません');
+        }
+        $program_id = $_GET['program_id'];
+
+        try {
+            $prec = new DBRecord(PROGRAM_TBL, "id", $program_id);
+
+            sscanf($prec->starttime, "%4d-%2d-%2d %2d:%2d:%2d", $syear, $smonth, $sday, $shour, $smin, $ssec);
+            sscanf($prec->endtime, "%4d-%2d-%2d %2d:%2d:%2d", $eyear, $emonth, $eday, $ehour, $emin, $esec);
+
+            $crecs = DBRecord::createRecords(CATEGORY_TBL);
+            $cats = array();
+            foreach ($crecs as $crec) {
+                $cat = array();
+                $cat['id'] = $crec->id;
+                $cat['name'] = $crec->name_jp;
+                $cat['selected'] = $prec->category_id == $cat['id'] ? "selected" : "";
+                array_push($cats, $cat);
+            }
+
+            $smarty = new Smarty();
+            $smarty->assign( "syear", $syear );
+            $smarty->assign( "smonth", $smonth );
+            $smarty->assign( "sday", $sday );
+            $smarty->assign( "shour", $shour );
+            $smarty->assign( "smin" ,$smin );
+            $smarty->assign( "eyear", $eyear );
+            $smarty->assign( "emonth", $emonth );
+            $smarty->assign( "eday", $eday );
+            $smarty->assign( "ehour", $ehour );
+            $smarty->assign( "emin" ,$emin );
+            $smarty->assign( "type", $prec->type );
+            $smarty->assign( "channel", $prec->channel );
+            $smarty->assign( "channel_id", $prec->channel_id );
+            $smarty->assign( "record_mode" , $RECORD_MODE );
+            $smarty->assign( "title", $prec->title );
+            $smarty->assign( "description", $prec->description );
+            $smarty->assign( "cats" , $cats );
+            $smarty->assign( "program_id", $prec->id );
+            $smarty->display("reservationform.html");
+        } catch (Exception $e) {
+            exit("Error:". $e->getMessage());
+        }
+    }
 }
 
 // dispatch
