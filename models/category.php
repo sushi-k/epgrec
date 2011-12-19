28cebfb3d94b28e81f3ed8f947ad5cdc99f177c4
ha1t <halt.feits@gmail.com>
2011-12-20 00:52:30 +0900

    getAll()追加

--- models/category.php
+++ models/category.php
@@ -5,13 +5,27 @@
  */
 class Category extends Model
 {
+    const TABLE = 'Recorder_categoryTbl';
+
     public static function get($category_disc)
     {
         $db = DB::conn();
-        $row = $db->row('SELECT * FROM Recorder_categoryTbl WHERE category_disc = ?', array($category_disc));
+        $row = $db->row('SELECT * FROM ' . self::TABLE . ' WHERE category_disc = ?', array($category_disc));
         if ($row === false) {
             return false;
         }
         return new self($row);
+    }
+
+    public static function getAll()
+    {
+        $items = array();
+        $db = DB::conn();
+        $rows = $db->rows('SELECT * FROM ' . self::TABLE . ' ORDER BY name_jp');
+        foreach ($rows as $row) {
+            $items[] = new self($row);
+        }
+
+        return $items;
     }
 }
