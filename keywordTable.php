6d5998aa7ba89eef4e1b03f632469dbe886fc4e3
ha1t <halt.feits@gmail.com>
2011-12-19 12:32:19 +0900

    DBRecordの処理を一部置き換え

--- keywordTable.php
+++ keywordTable.php
@@ -1,10 +1,9 @@
 <?php
-include_once('config.php');
-include_once( INSTALL_PATH . '/Smarty/Smarty.class.php' );
-include_once( INSTALL_PATH . "/DBRecord.class.php" );
-include_once( INSTALL_PATH . "/Reservation.class.php" );
-include_once( INSTALL_PATH . "/Keyword.class.php" );
-// include_once( INSTALL_PATH . "/Settings.class.php" );
+require_once 'config.php';
+require_once INSTALL_PATH . '/Smarty/Smarty.class.php';
+require_once INSTALL_PATH . '/DBRecord.class.php';
+require_once INSTALL_PATH . '/Reservation.class.php';
+require_once INSTALL_PATH . '/Keyword.class.php';
 
 $weekofdays = array( "月", "火", "水", "木", "金", "土", "日", "なし" );
 
@@ -23,51 +22,40 @@ if( isset($_POST["add_keyword"]) ) {
 			
 			// 録画予約実行
 			$rec->reservation();
-		}
-		catch( Exception $e ) {
+		} catch( Exception $e ) {
 			exit( $e->getMessage() );
 		}
 	}
 }
 
-
-$keywords = array();
 try {
-	$recs = Keyword::createRecords(KEYWORD_TBL);
-	foreach( $recs as $rec ) {
-		$arr = array();
-		$arr['id'] = $rec->id;
-		$arr['keyword'] = $rec->keyword;
-		$arr['type'] = $rec->type == "*" ? "すべて" : $rec->type;
+	$db = DB::conn();
+	$keywords = $db->rows("SELECT * FROM Recorder_keywordTbl");
+	foreach ($keywords as $key => $keyword) {
+		$keywords[$key]['type'] = $keyword['type'] == "*" ? "すべて" : $keyword['type'];
 		
-		if( $rec->channel_id ) {
-			$crec = new DBRecord(CHANNEL_TBL, "id", $rec->channel_id );
-			$arr['channel'] = $crec->name;
+		$keywords[$key]['channel'] = 'すべて';
+		if ($keyword['channel_disc']) {
+			$crec = new DBRecord(CHANNEL_TBL, "id", $keyword['channel_disc']);
+			$keywords[$key]['channel'] = $crec->name;
 		}
-		else $arr['channel'] = 'すべて';
-		
-		if( $rec->category_id ) {
-			$crec = new DBRecord(CATEGORY_TBL, "id", $rec->category_id );
-			$arr['category'] = $crec->name_jp;
+
+		$keywords[$key]['category'] = 'すべて';
+		if ($keyword['category_disc']) {
+			$crec = new DBRecord(CATEGORY_TBL, "id", $keyword['category_disc']);
+			$keywords[$key]['category'] = $crec->name_jp;
 		}
-		else $arr['category'] = 'すべて';
-		
-		$arr['use_regexp'] = $rec->use_regexp;
-		
-		$arr['weekofday'] = $weekofdays["$rec->weekofday"];
-		
-		$arr['autorec_mode'] = $RECORD_MODE[(int)$rec->autorec_mode]['name'];
-		
-		array_push( $keywords, $arr );
+
+		$keywords[$key]['weekofday'] = $weekofdays[$keyword['weekofday']];
+
+		$keywords[$key]['autorec_mode'] = $RECORD_MODE[(int)$keyword['autorec_mode']]['name'];
 	}
-}
-catch( Exception $e ) {
+} catch( Exception $e ) {
 	exit( $e->getMessage() );
 }
 
 $smarty = new Smarty();
+$smarty->assign('keywords', $keywords);
+$smarty->assign('sitetitle', '自動録画キーワードの管理');
+$smarty->display('keywordTable.html');
 
-$smarty->assign( "keywords", $keywords );
-$smarty->assign( "sitetitle", "自動録画キーワードの管理" );
-$smarty->display( "keywordTable.html" );
-?>
