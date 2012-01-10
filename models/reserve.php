65ef54d4fa8870a423002050851582adba460c5c
ha1t <halt.feits@gmail.com>
2012-01-10 21:59:36 +0900

    録画済み一覧の表示処理を修正

--- models/reserve.php
+++ models/reserve.php
@@ -20,6 +20,7 @@ class Reserve
 
     public static function getRecordedItems($options = array())
     {
+        $now = date("Y-m-d");
         $db = DB::conn();
         $table = self::TABLE;
         $program_table  = Program::TABLE;
@@ -27,9 +28,10 @@ class Reserve
         $channel_table  = Channel::TABLE;
         $sql = <<<EOD
 SELECT * FROM {$table}
-  LEFT JOIN {$program_table} ON {$table}.program_disc = {$program_table}.program_disc
+  INNER JOIN {$program_table} ON {$table}.program_disc = {$program_table}.program_disc
   LEFT JOIN {$category_table} ON {$category_table}.category_disc = {$program_table}.category_disc
   LEFT JOIN {$channel_table} ON {$channel_table}.channel_disc = {$program_table}.channel_disc
+WHERE endtime < "{$now}"
 ORDER BY starttime DESC
 EOD;
         return $db->rows($sql);
