83207955dd576aeb65876219b08775a80735b35e
ha1t <halt.feits@gmail.com>
2012-01-19 14:37:13 +0900

    キーワード削除機能を動くようにした

--- api.php
+++ api.php
@@ -74,16 +74,13 @@ EOD;
 
     public function deleteKeyword()
     {
-        require_once INSTALL_PATH . '/Keyword.class.php';
-
         if (!isset($_GET['keyword_id'])) {
             exit("Error:キーワードIDが指定されていません");
         }
 
         try {
-            $rec = new Keyword( "id", $_GET['keyword_id']);
-            $rec->delete();
-        } catch(Exception $e ) {
+            Keyword::delete((int)$_GET['keyword_id']);
+        } catch (Exception $e) {
             exit("Error:" . $e->getMessage());
         }
     }
