8506d17d649e67ab5bfd093f0a18b7557421bff1
ha1t <halt.feits@gmail.com>
2012-01-19 13:49:02 +0900

    retab

--- keywordTable.php
+++ keywordTable.php
@@ -9,49 +9,49 @@ $weekofdays = array( "月", "火", "水", "木", "金", "土", "日", "なし" )
 
 // 新規キーワードがポストされた
 if( isset($_POST["add_keyword"]) ) {
-	if( $_POST["add_keyword"] == 1 ) {
-		try {
-			$rec = new Keyword();
-			$rec->keyword = $_POST['k_search'];
-			$rec->type = $_POST['k_type'];
-			$rec->category_id = $_POST['k_category'];
-			$rec->channel_id = $_POST['k_station'];
-			$rec->use_regexp = $_POST['k_use_regexp'];
-			$rec->weekofday = $_POST['k_weekofday'];
-			$rec->autorec_mode = $_POST['autorec_mode'];
-			
-			// 録画予約実行
-			$rec->reservation();
-		} catch( Exception $e ) {
-			exit( $e->getMessage() );
-		}
-	}
+    if( $_POST["add_keyword"] == 1 ) {
+        try {
+            $rec = new Keyword();
+            $rec->keyword = $_POST['k_search'];
+            $rec->type = $_POST['k_type'];
+            $rec->category_id = $_POST['k_category'];
+            $rec->channel_id = $_POST['k_station'];
+            $rec->use_regexp = $_POST['k_use_regexp'];
+            $rec->weekofday = $_POST['k_weekofday'];
+            $rec->autorec_mode = $_POST['autorec_mode'];
+
+            // 録画予約実行
+            $rec->reservation();
+        } catch( Exception $e ) {
+            exit( $e->getMessage() );
+        }
+    }
 }
 
 try {
-	$db = DB::conn();
-	$keywords = $db->rows("SELECT * FROM Recorder_keywordTbl");
-	foreach ($keywords as $key => $keyword) {
-		$keywords[$key]['type'] = $keyword['type'] == "*" ? "すべて" : $keyword['type'];
-		
-		$keywords[$key]['channel'] = 'すべて';
-		if ($keyword['channel_disc']) {
-			$crec = new DBRecord(CHANNEL_TBL, "id", $keyword['channel_disc']);
-			$keywords[$key]['channel'] = $crec->name;
-		}
+    $db = DB::conn();
+    $keywords = $db->rows("SELECT * FROM Recorder_keywordTbl");
+    foreach ($keywords as $key => $keyword) {
+        $keywords[$key]['type'] = $keyword['type'] == "*" ? "すべて" : $keyword['type'];
 
-		$keywords[$key]['category'] = 'すべて';
-		if ($keyword['category_disc']) {
-			$crec = new DBRecord(CATEGORY_TBL, "id", $keyword['category_disc']);
-			$keywords[$key]['category'] = $crec->name_jp;
-		}
+        $keywords[$key]['channel'] = 'すべて';
+        if ($keyword['channel_disc']) {
+            $crec = new DBRecord(CHANNEL_TBL, "id", $keyword['channel_disc']);
+            $keywords[$key]['channel'] = $crec->name;
+        }
 
-		$keywords[$key]['weekofday'] = $weekofdays[$keyword['weekofday']];
+        $keywords[$key]['category'] = 'すべて';
+        if ($keyword['category_disc']) {
+            $crec = new DBRecord(CATEGORY_TBL, "id", $keyword['category_disc']);
+            $keywords[$key]['category'] = $crec->name_jp;
+        }
 
-		$keywords[$key]['autorec_mode'] = $RECORD_MODE[(int)$keyword['autorec_mode']]['name'];
-	}
+        $keywords[$key]['weekofday'] = $weekofdays[$keyword['weekofday']];
+
+        $keywords[$key]['autorec_mode'] = $RECORD_MODE[(int)$keyword['autorec_mode']]['name'];
+    }
 } catch( Exception $e ) {
-	exit( $e->getMessage() );
+    exit( $e->getMessage() );
 }
 
 $smarty = new Smarty();
