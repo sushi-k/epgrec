2e8f0d3188da5841ab1b0250ef2393e3163e4cc3
ha1t <halt.feits@gmail.com>
2012-02-03 01:19:36 +0900

    Setting系は外す予定だけど一応移植

--- webroot/systemSetting.php
+++ webroot/systemSetting.php
@@ -1,10 +1,11 @@
 <?php
-require_once("config.php");
-require_once(INSTALL_PATH."/Smarty/Smarty.class.php");
+require_once dirname(dirname(__FILE__) ) . "/config.php";
 require_once(INSTALL_PATH."/Settings.class.php");
 
 $settings = Settings::factory();
 $smarty = new Smarty();
+$smarty->template_dir = dirname(dirname(__FILE__)) . '/templates/'; 
+$smarty->compile_dir = dirname(dirname(__FILE__)) . '/templates_c/'; 
 $smarty->assign( "settings", $settings );
 $smarty->assign( "install_path", INSTALL_PATH );
 $smarty->assign( "sitetitle", "システム設定" );
